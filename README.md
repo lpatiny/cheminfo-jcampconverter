b95de0241c0f0b4510bcf611a30d1526da5be873
lpatiny <luc@patiny.com>
2021-07-12 20:49:27 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-wy8c6v
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-wy8c6v)
---
+++ index.js
@@ -0,0 +1,5 @@
+import {MF} from 'mass-tools';
+
+// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
